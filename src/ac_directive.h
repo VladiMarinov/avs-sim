57b5490214d75b98469d458545c7cdd84bd3e1f1
m0rtal <vladi.github@gmail.com>
2021-05-12 12:47:40 +0300

    added AC_Directive class

---
+++ src/ac_directive.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class AC_Directive
+{
+public:
+  std::string sweep_type;
+  std::string points_per_dec;
+  std::string start_freq;
+  std::string stop_freq;
+};
