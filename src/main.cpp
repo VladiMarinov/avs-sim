7f2d888c6bc4b3977a374207e7a1ed8c3705a56a
m0rtal <vladi.github@gmail.com>
2021-05-13 17:14:51 +0300

    removed vector of strings for decoding Types

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,10 @@
 #include <string>
 #include "parser.h" 
 #include "circuit.h"
-
-std::vector<std::string> componentType = {
-    "RESISTOR",       //0   R
-    "INDUCTOR",       //1   L
-    "CAPACITOR",      //2   C
-    "VOLTAGE_SOURCE", //3   V
-    "CURRENT_SOURCE", //4   I
-    "DIODE",          //5   D
-    "BJT",            //6   Q
-    "MOSFET",         //7   M
-    "VCCS"            //8   G
-  };
-  
+ 
 void printComponent(Component c)
 {
   std::cout <<"----------------------------\n";
-  std::cout << componentType[c.type] << " with enum " << c.type << std::endl;
   std::cout << c.designator << std::endl;
   for(std::string n : c.nodes)
   {
@@ -37,14 +24,14 @@ void printACdir(AC_Directive dir)
   std::cout <<"----------------------------\n";
 }
 
-// void printNode(Node node){
-//   std::cout << node.name;
-//   for(Component c : node.components)
-//   {
-//     std::cout << "- " << c.designator;
-//   }
-//   std::cout << std::endl;
-// }
+void printNode(Node node){
+  std::cout << node.name;
+  for(Component c : node.components)
+  {
+    std::cout << " " << c.designator;
+  }
+  std::cout << std::endl;
+}
 
 int main(int argc, char** argv)
 {
@@ -58,17 +45,17 @@ int main(int argc, char** argv)
     parser->parse();
     for(Component c : parser->components)
     {
-      printComponent(c);
+      //printComponent(c);
     }
     printACdir(parser->ac_dir);
 
     std::cout <<"\n----------------------------\n";
 
     Circuit circuit(parser->components);
-    // for(Node node : circuit.nodes)
-    // {
-    //   node.printNode();
-    // }
+    for(Node node : circuit.nodes)
+    {
+      printNode(node);
+    }
 
     return 0;
 }
