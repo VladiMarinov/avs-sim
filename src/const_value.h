2fa8049180aab0ebf4533de8f2339d57ac6c6b6d
sv720 <sv720@ic.ac.uk>
2021-05-20 16:45:07 +0100

    const_value.h NOT SAVED ON LAST PUSH

---
+++ src/const_value.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <cctype>
+
+
+class Const_value
+{
+public: 
+    std::string str_value; 
+    float numeric_value;
+    
+    Const_value(std::string input_str_value);
+
+    float str_to_numeric(std::string input_str_value);
+    bool is_single_character_multipler(char c);
+    float str_to_multiplier(std::string multiplier);
+};
