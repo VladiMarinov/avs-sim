51701acf2f68c19cb1439bc5374d6304b78a4f73
m0rtal <vladi.github@gmail.com>
2021-05-21 17:21:12 +0300

    created Model_value class

---
+++ src/model_value.cpp
@@ -0,0 +1,6 @@
+#include "model_value.h"
+
+Model_value::Model_value(std::string imodel)
+{
+  model_name = imodel;
+}
---
+++ src/model_value.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+class Model_value
+{
+public:
+  std::string model_name;
+  Model_value(std::string imodel);
+};
