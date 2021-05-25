429b242ecc7adeea878efcd32417a9b54c0b236e
m0rtal <vladi.github@gmail.com>
2021-05-25 20:35:49 +0300

    added models:: namespace with structs for each supported model

---
+++ src/models.h
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace models
+{
+
+struct D
+{
+  const double IS = 1e-14;
+  const double VT = 0.026; 
+  const double initial_V_guess = 0.7;
+};
+
+/// BASED ON LTSPICE 2N2222
+struct NPN
+{
+  const double IS = 1e-14;
+  const double BF = 200.0;
+  const double BR = 3.0;
+  const double VT = 0.026;
+
+  const double initial_Vbe_guess = 0.7;
+  const double initial_Vbc_guess = -1.0;
+
+  double AF;
+  double AR;
+  double IES;
+  double ICS;
+  NPN()
+  {
+    AF = BF / (BF + 1);
+    AR = BR / (BR + 1);
+    IES = IS / AF;
+    ICS = IS / AR;
+  }
+};
+
+}
+
+
