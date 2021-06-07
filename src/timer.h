5d754cc21e690a84daac93260420bfde190431fc
m0rtal <vladi.github@gmail.com>
2021-06-07 19:31:59 +0300

    added doxy comments

--- src/timer.h
+++ src/timer.h
@@ -4,15 +4,24 @@
 #include<chrono>
 #include<string>
 
+/// Useful classes for benchmarking
 namespace benchmark{
 
+/// Lifetime-based timer for benchmarking.
+/// Will print its lifetime when destructor called.
+/// Scopes can be used for easy lifetime setting.
 class Timer{
 private:
     std::string finished_message;
     std::chrono::time_point < std::chrono::high_resolution_clock > start_time_point;
 public:
+    /// Create a timer with default finished message (TIMER:)
     Timer();
+
+    /// Create a timer with the given finished message
     Timer(std::string i_finished_message);
+
+    /// When the destructor is called the timer will print its lifetime...
     ~Timer();
 };
 
