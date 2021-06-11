799d803e63dd75210681579c2f3217f997ee152f
m0rtal <vladi.github@gmail.com>
2021-06-12 02:33:09 +0300

    more clang-tidy performance suggestions

--- src/function_value.cpp
+++ src/function_value.cpp
@@ -4,9 +4,9 @@
 #include <utility>
 
 
-Function_value::Function_value(std::string itype, std::string iamp, std::string iphase):
-  amplitude(std::move(iamp)),
-  phase(std::move(iphase))
+Function_value::Function_value(std::string itype, const std::string& iamp, const std::string& iphase):
+  amplitude(iamp),
+  phase(iphase)
 {
   function_type = std::move(itype);
 }
